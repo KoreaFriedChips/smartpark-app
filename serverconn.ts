0ce42e730e785bed7cbd38af3e8da740c944f721
Raine Soriano <rainearcher@g.ucla.edu>
2024-04-22 10:53:41 -0700

    Send token in header

--- serverconn.ts
+++ serverconn.ts
@@ -5,10 +5,12 @@ export const signin = async (t: string) => { return await sendToServer(t, "/api/
 const sendToServer = async (token: string, path: string, method: string, data: any, params: any) => {
     const env = Constants.expoConfig?.extra;
     const serverUrl = env?.serverURL;
-    data.token = token;
-    console.log(JSON.stringify(data));
     const searchParams = buildSearchParams(params).toString();
-    const res = await fetch(serverUrl + path + searchParams, { method, body: JSON.stringify(data) })
+    const res = await fetch(serverUrl + path + "?" + searchParams, {
+        method: method,
+        headers: { token: token },
+        body: data ? JSON.stringify(data): undefined,
+    });
     if (res.status != 200) throw new Error((await res.json()).error);
     return res;
 }
@@ -19,131 +21,152 @@ const buildSearchParams = (params: any) => {
     return searchParams;
 }
 
+const create = async (token: string, path: string, data: any) => {
+    const res = await sendToServer(token, path, "POST", data, {});
+    const resData = await res.json();
+    return resData.data;
+}
+
+const read = async (token: string, path: string, searchParams: any) => {
+    const res =  await sendToServer(token, path, "GET", undefined, searchParams);
+    const resData = await res.json();
+    return resData.data;
+}
+
+const update = async (token: string, path: string, data: any) => {
+    const res = await sendToServer(token, path, "PUT", data, {});
+    return await res.json();
+}
+
+const serverDelete = async (token: string, path: string) => {
+    const res = await sendToServer(token, path, "DELETE", undefined, {});
+    return await res.json();
+}
+
 export const createUser = async(token: string, data: UserCreate) => {
-    return await sendToServer(token, "/api/users", "POST", data, {});
+    return await create(token, "/api/users", data);
 }
 
 export const readUsers = async( token: string, searchParams: UserParams ) => {
-    return await sendToServer(token, "/api/users", "GET", {}, searchParams);
+    return await read(token, "/api/users", searchParams);
 } 
 
 export const updateUser = async( token: string, id: string, data: UserParams ) => {
-    return await sendToServer(token, `/api/users/${id}`, "PUT", data, {});
+    return await update(token, `/api/users/${id}`, data);
 }
 
 export const deleteUser = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/users/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/users/${id}`);
 }
 
 export const createListing = async(token: string, data: ListingCreate) => {
-    return await sendToServer(token, "/api/listings", "POST", data, {});
+    return await create(token, "/api/listings", data);
 }
 
 export const readListings = async( token: string, searchParams: ListingParams ) => {
-    return await sendToServer(token, "/api/listings", "GET", {}, searchParams);
+    return await read(token, "/api/listings", searchParams);
 } 
 
 export const updateListing = async( token: string, id: string, data: ListingParams ) => {
-    return await sendToServer(token, `/api/listings/${id}`, "PUT", data, {});
+    return await update(token, `/api/listings/${id}`, data);
 }
 
 export const deleteListing = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/listings/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/listings/${id}`);
 }
 
-
 export const createReview = async(token: string, data: ReviewCreate) => {
-    return await sendToServer(token, "/api/reviews", "POST", data, {});
+    return await create(token, "/api/reviews", data);
 }
 
 export const readReviews = async( token: string, searchParams: ReviewParams ) => {
-    return await sendToServer(token, "/api/reviews", "GET", {}, searchParams);
+    return await read(token, "/api/reviews", searchParams);
 } 
 
 export const updateReview = async( token: string, id: string, data: ReviewParams ) => {
-    return await sendToServer(token, `/api/reviews/${id}`, "PUT", data, {});
+    return await update(token, `/api/reviews/${id}`, data);
 }
 
 export const deleteReview = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/reviews/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/reviews/${id}`);
 }
 
 export const createWaitlist = async(token: string, data: WaitlistCreate) => {
-    return await sendToServer(token, "/api/waitlists", "POST", data, {});
+    return await create(token, "/api/waitlists", data);
 }
 
 export const readWaitlists = async( token: string, searchParams: WaitlistParams ) => {
-    return await sendToServer(token, "/api/waitlists", "GET", {}, searchParams);
+    return await read(token, "/api/waitlists", searchParams);
 } 
 
 export const updateWaitlist = async( token: string, id: string, data: WaitlistParams ) => {
-    return await sendToServer(token, `/api/waitlists/${id}`, "PUT", data, {});
+    return await update(token, `/api/waitlists/${id}`, data);
 }
 
 export const deleteWaitlist = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/waitlists/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/waitlists/${id}`);
 }
 
 export const createBid = async(token: string, data: BidCreate) => {
-    return await sendToServer(token, "/api/bids", "POST", data, {});
+    return await create(token, "/api/bids", data);
 }
 
 export const readBids = async( token: string, searchParams: BidParams ) => {
-    return await sendToServer(token, "/api/bids", "GET", {}, searchParams);
+    return await read(token, "/api/bids", searchParams);
 } 
 
 export const updateBid = async( token: string, id: string, data: BidParams ) => {
-    return await sendToServer(token, `/api/bids/${id}`, "PUT", data, {});
+    return await update(token, `/api/bids/${id}`, data);
 }
 
 export const deleteBid = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/bids/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/bids/${id}`);
 }
 
 export const createTransaction = async(token: string, data: TransactionCreate) => {
-    return await sendToServer(token, "/api/transactions", "POST", data, {});
+    return await create(token, "/api/transactions", data);
 }
 
 export const readTransactions = async( token: string, searchParams: TransactionParams ) => {
-    return await sendToServer(token, "/api/transactions", "GET", {}, searchParams);
+    return await read(token, "/api/transactions", searchParams);
 } 
 
 export const updateTransaction = async( token: string, id: string, data: TransactionParams ) => {
-    return await sendToServer(token, `/api/transactions/${id}`, "PUT", data, {});
+    return await update(token, `/api/transactions/${id}`, data);
 }
 
 export const deleteTransaction = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/transactions/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/transactions/${id}`);
 }
 
 export const createFavorite = async(token: string, data: FavoriteCreate) => {
-    return await sendToServer(token, "/api/favorites", "POST", data, {});
+    return await create(token, "/api/favorites", data);
 }
 
 export const readFavorites = async( token: string, searchParams: FavoriteParams ) => {
-    return await sendToServer(token, "/api/favorites", "GET", {}, searchParams);
+    return await read(token, "/api/favorites", searchParams);
 } 
 
 export const updateFavorite = async( token: string, id: string, data: FavoriteParams ) => {
-    return await sendToServer(token, `/api/favorites/${id}`, "PUT", data, {});
+    return await update(token, `/api/favorites/${id}`, data);
 }
 
 export const deleteFavorite = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/favorites/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/favorites/${id}`);
 }
 
 export const createConfirmation = async(token: string, data: ConfirmationCreate) => {
-    return await sendToServer(token, "/api/confirmations", "POST", data, {});
+    return await create(token, "/api/confirmations", data);
 }
 
 export const readConfirmations = async( token: string, searchParams: ConfirmationParams ) => {
-    return await sendToServer(token, "/api/confirmations", "GET", {}, searchParams);
+    return await read(token, "/api/confirmations", searchParams);
 } 
 
 export const updateConfirmation = async( token: string, id: string, data: ConfirmationParams ) => {
-    return await sendToServer(token, `/api/confirmations/${id}`, "PUT", data, {});
+    return await update(token, `/api/confirmations/${id}`, data);
 }
 
 export const deleteConfirmation = async ( token: string, id: string ) => {
-    return await sendToServer(token, `/api/confirmations/${id}`, "DELETE", {}, {});
+    return await serverDelete(token, `/api/confirmations/${id}`);
 }
