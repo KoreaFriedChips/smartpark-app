39144d0024e9ccd235b043a97255219caac4affa
Raine Soriano <rainearcher@g.ucla.edu>
2024-04-22 15:31:00 -0700

    Draft add parking spot wizard

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,14 +1,185 @@
-import { Modal, StyleSheet } from 'react-native';
+import { Modal, ScrollView, StyleSheet, useColorScheme, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput } from '@/components/Themed';
+import React, { useState } from 'react';
+import MapView, { Address, LatLng, Marker, Region } from 'react-native-maps';
+import { Dimensions } from 'react-native';
+import Colors from '@/constants/Colors';
+import { listingData } from '@/components/utils/ListingData';
+import { createListing, readUsers } from '@/serverconn';
+import { useAuth } from '@clerk/clerk-expo';
+import { Picker } from '@react-native-picker/picker';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+interface Availability {
+  day: string,
+  availableHours: string[],
+  isAvailable: boolean,
+}
 
-export default function TabTwoScreen() {
+export default function CreateListing() {
+  const themeColors = Colors[useColorScheme() || "light"];
+  const [region, setRegion] = useState<Region>({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.00922,
+    longitudeDelta: 0.00421,
+  });
+
+  const [spotCoords, setSpotCoords] = useState<LatLng>({
+    latitude: 37.78825,
+    longitude: -122.4324
+  });
+  const [spotAddress, setSpotAddress] = useState<Address>();
+
+  const { isLoaded, isSignedIn, userId, signOut, getToken } = useAuth();
+  const [sellerId, setSellerId] = useState("");
+  React.useEffect(() => {
+    const getId = async () => {
+      try {
+        const users: any = await readUsers(await getToken() ?? "", { clerkId: userId || "" });
+        setSellerId(users[0].id);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getId();
+  }, []);
+
+
+  const verifyListingData = (listingData: ListingCreate) => {
+    return true;
+  }
+  const handleSubmitCreateListing = React.useCallback(async (listingData: ListingCreate) => {
+    console.log(listingData);
+    if (!verifyListingData(listingData)) {
+      return;
+    }
+    await createListing(await getToken() ?? "", listingData);
+  }, [ listingData ]);
+
+
+  const [listingType, setListingType] = useState("Parking Spot");
+  const [price, setPrice] = useState("");
+  const [duration, setDuration] = useState("hour");
+  const [availability, setAvailability] = useState<Array<Availability>>([]);
+  const [description, setDescription] = useState("");
+
+  const mapRef = React.useRef<MapView>(null);
+  const handleAddressChange = async (coord: LatLng) => {
+    setSpotAddress(await mapRef.current?.addressForCoordinate(coord));
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <ScrollView style={styles.scroll}>
+        <Text style={styles.title}>Create Listing</Text>
+        <View style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          <MapView
+          ref={mapRef}
+            style={[styles.map, { borderColor: themeColors.outline }]}
+            zoomEnabled={false}
+            region={region}
+            onRegionChangeComplete={setRegion}
+            onPress={(event) => {
+              setSpotCoords(event.nativeEvent.coordinate);
+              handleAddressChange(event.nativeEvent.coordinate);
+            }}
+            showsCompass={false}>
+            <Marker coordinate={spotCoords}>
+
+            </Marker>
+          </MapView>
+        </View>
+        <Text weight="semibold" style={{ color: themeColors.third }}> Description </Text>
+        <TextInput
+          style={[styles.input, {borderColor: themeColors.outline}]}
+          placeholder="Description"
+          onChangeText={(text) => setDescription(text)}
+          value={description}
+          keyboardType="default"
+          returnKeyType="next"
+          clearButtonMode="always"
+        />
+        <Text weight="semibold" style={{ color: themeColors.third }}> Price </Text>
+        <TextInput
+          style={[styles.input, {borderColor: themeColors.outline}]}
+          placeholder="Price"
+          onChangeText={(text) => setPrice(text)}
+          value={price}
+          keyboardType="decimal-pad"
+          returnKeyType="next"
+          clearButtonMode="always"
+        />
+        <Text weight="semibold" style={{ color: themeColors.third }}> Listing Type </Text>
+        <View style={{...styles.input, padding: 0, borderColor: themeColors.outline}}>
+          <Picker
+            itemStyle={{
+              color: themeColors.primary,
+              fontFamily: "Soliden-Medium",
+              letterSpacing: -0.5,
+              fontSize: 18,
+            }}
+            selectedValue={listingType}
+            onValueChange={(itemValue) => setListingType(itemValue)}
+          >
+            <Picker.Item label="Parking Spot" value="Parking Spot"/>
+          </Picker>
+        </View>
+        <Text weight="semibold" style={{ color: themeColors.third }}> Duration </Text>
+        <View style={{...styles.input, padding: 0, borderColor: themeColors.outline}}>
+          <Picker
+            itemStyle={{
+              color: themeColors.primary,
+              fontFamily: "Soliden-Medium",
+              letterSpacing: -0.5,
+              fontSize: 18,
+            }}
+            selectedValue={duration}
+            onValueChange={(itemValue) => setDuration(itemValue)}
+          >
+            <Picker.Item label="Minute" value="minute"/>
+            <Picker.Item label="Hour" value="hour"/>
+            <Picker.Item label="Day" value="day"/>
+            <Picker.Item label="Week" value="week"/>
+            <Picker.Item label="Month" value="month"/>
+          </Picker>
+        </View>
+        <TouchableOpacity 
+          style={[
+            styles.button,
+           {
+              backgroundColor: Colors['accent'],
+              borderColor: Colors['accentAlt'],
+              marginTop: 16
+            }]
+          }
+          onPress={async () => handleSubmitCreateListing({
+            thumbnail: "",
+            images: [],
+            latitude: spotCoords.latitude,
+            longitude: spotCoords.longitude,
+            city: spotAddress?.locality ?? "",
+            state: spotAddress?.administrativeArea ?? "",
+            listingType: listingType,
+            price: Number(price),
+            duration: duration,
+            description: description,
+            availability: availability,
+            date: new Date(),
+            sellerId: sellerId,
+          })}
+        >
+        <Text
+          weight="bold"
+          style={{
+            ...styles.buttonText,
+            color: Colors["light"].primary,
+          }}
+        >
+            Create Listing
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      </ScrollView>
     </View>
   );
 }
@@ -28,4 +199,48 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  scroll: {
+    width: Dimensions.get("window").width,
+    paddingTop: 12,
+    padding: 16,
+  },
+  map: {
+    width: Dimensions.get("window").width - 32,
+    height: 300,
+    marginTop: 22,
+    marginBottom: 42,
+    borderRadius: 8,
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,
+    elevation: 3,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 4,
+    marginTop: 12,
+    marginBottom: 4,
+    borderWidth: 1,
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    textAlign: "center",
+  },
+  input: {
+    width: "100%",
+    marginTop: 5,
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+  }
 });
