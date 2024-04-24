075b2b8c5ce536c0325865d0a9368904b3cda54f
Raine Soriano <rainearcher@g.ucla.edu>
2024-04-24 14:14:28 -0700

    Add Carousel support to listing gallery

--- components/ListingGallery.tsx
+++ components/ListingGallery.tsx
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
-import { StyleSheet, Image, useColorScheme } from "react-native";
+import { StyleSheet, Image, useColorScheme, Dimensions } from "react-native";
 import { Text, View } from "@/components/Themed";
 import Colors from "@/constants/Colors";
 import HeartButton from "@/components/ListingCard/HeartButton";
 import DistanceText from "@/components/ListingCard/DistanceText";
+import SideSwipe from 'react-native-sideswipe';
+
 
 export default function ListingGallery( { listing }: {listing: Listing}) {
   const themeColors = Colors[useColorScheme() || "light"];
   const { distance } = useLocalSearchParams();
-  return(<View>
-    <Image source={{ uri: listing.thumbnail }} style={[styles.thumbnail, { borderColor: themeColors.outline }]} />
-  <HeartButton
-    id={listing.id}
-    style={{ top: 24, right: 10 }}
-  />
-  <DistanceText
-    distance={Number(distance)}
-    style={{ top: 32, left: 18 }}
-  /> 
-  </View>);
+  const width = Dimensions.get('window').width;
+  const [index, setIndex] = useState(0);
+  return(
+    <View>
+      <SideSwipe 
+        index={index}
+        style={{width}}
+        data={listing.images}
+        onIndexChange={setIndex}
+        renderItem={({itemIndex, currentIndex, item, animatedValue}) => (
+          <Image 
+            source={{uri: item}}
+            style={[styles.thumbnail, { width: width, borderColor: themeColors.outline }]}
+          />
+        )}
+      />
+        <HeartButton
+          id={listing.id}
+          style={{ top: 24, right: 10 }}
+        />
+      <DistanceText
+        distance={Number(distance)}
+        style={{ top: 32, left: 18 }}
+      /> 
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   thumbnail: {
-    width: "100%",
     height: 350,
     borderRadius: 8,
     borderWidth: 1,
