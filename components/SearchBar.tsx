2b6e82559ffe25724790f8161ae514999090d37a
Raine Soriano <rainearcher@g.ucla.edu>
2024-05-08 13:28:28 -0700

    Track up to 5 previous searches

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { StyleSheet, useColorScheme, Dimensions, NativeSyntheticEvent, TextInputSubmitEditingEventData } from "react-native";
-import { View, TextInput } from "@/components/Themed";
+import { StyleSheet, useColorScheme, Dimensions, NativeSyntheticEvent, TextInputSubmitEditingEventData, TouchableOpacity } from "react-native";
+import { View, TextInput, Text } from "@/components/Themed";
 import Colors from "@/constants/Colors";
 import { Search } from "lucide-react-native";
 
@@ -12,30 +12,63 @@ interface SearchBarProps {
 
 export default function SearchBar({searchQuery, setSearchQuery, onSubmitEditing}: SearchBarProps) {
   const themeColors = Colors[useColorScheme() || "light"];
+  const maxPrevSearches = 5;
+  const [prevSearches, setPrevSearches] = useState<string[]>([]);
+  const [showPrevSearches, setShowPrevSearches] = useState(false);
+
+  const addPrevSearch = (text: string) => {
+    let newSearches = [text, ...prevSearches];
+    if (newSearches.length > maxPrevSearches) {
+      newSearches.pop();
+    }
+    setPrevSearches(newSearches);
+  }
 
   return (
+    <View style={[styles.container,{ backgroundColor: themeColors.background, borderColor: themeColors.outline } ]}>
     <View style={[styles.searchContainer, { backgroundColor: themeColors.background, borderColor: themeColors.outline }]}>
       <Search size={18} color={themeColors.primary} strokeWidth={3} />
       <TextInput
         style={{...styles.searchBar}}
         placeholder="Search SmartPark.."
         onChangeText={setSearchQuery}
-        onSubmitEditing={onSubmitEditing}
+        onSubmitEditing={(e) => {addPrevSearch(e.nativeEvent.text); onSubmitEditing(e);}}
+        onFocus={() => setShowPrevSearches(true)}
+        onBlur={() => setShowPrevSearches(false)}
         value={searchQuery}
         keyboardType="default"
         returnKeyType="search"
         clearButtonMode="while-editing"
       />
     </View>
+    {showPrevSearches && <View style={styles.prevSearchContainer}>
+      {prevSearches.map((text, index) => (
+        <TouchableOpacity style={styles.prevSearchOption} key={index} onPress={() => setSearchQuery(text)}>
+          <Text>{text}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    borderRadius: 8,
+  },
   searchHeader: {
     height: 32,
     marginTop: 8,
     paddingVertical: 8,
     width: Dimensions.get("window").width - 96,
+  },
+  prevSearchContainer: {
+    marginBottom: 8
+  },
+  prevSearchOption: {
+    marginHorizontal: 8,
+    paddingHorizontal: 8,
+    marginVertical: 2
   },
   searchContainer: {
     flexDirection: "row",
